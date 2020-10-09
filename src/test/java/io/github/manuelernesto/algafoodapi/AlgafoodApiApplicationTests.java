1c596bcb5276e656ae8f185f9ab4af8afc34ec4d
codemage66 <kingstar3380@gmail.com>
2020-10-09 23:20:56 +0100

    creating the project

---
+++ src/test/java/io/github/manuelernesto/algafoodapi/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.manuelernesto.algafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
