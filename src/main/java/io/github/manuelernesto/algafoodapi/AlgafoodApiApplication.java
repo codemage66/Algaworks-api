1674b95561ab62ef95c1fd2dd2117ea32d39dddb
codemage66 <kingstar3380@gmail.com>
2020-10-21 10:41:24 +0100

    add jpa dependency

--- src/main/java/io/github/manuelernesto/algafoodapi/AlgafoodApiApplication.java
+++ src/main/java/io/github/manuelernesto/algafoodapi/AlgafoodApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgafoodApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AlgafoodApiApplication.class, args);
     }
-
 }
