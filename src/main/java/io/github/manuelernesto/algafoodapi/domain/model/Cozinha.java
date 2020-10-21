7ee7e80cce335ff76daefb7a25b3d1acc591b7df
codemage66 <kingstar3380@gmail.com>
2020-10-21 11:28:00 +0100

    creating the entities Cozinha.java and Restaurante.java

---
+++ src/main/java/io/github/manuelernesto/algafoodapi/domain/model/Cozinha.java
@@ -0,0 +1,50 @@
+package io.github.manuelernesto.algafoodapi.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+/**
+ * Mapeando Cozinha
+ */
+
+@Entity
+public class Cozinha {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cozinha cozinha = (Cozinha) o;
+
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/io/github/manuelernesto/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,54 @@
+package io.github.manuelernesto.algafoodapi.domain.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Restaurante that = (Restaurante) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
