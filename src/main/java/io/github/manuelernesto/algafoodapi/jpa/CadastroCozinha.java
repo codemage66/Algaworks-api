b5073b9021f1eb5d91359d05f62129f90f068e35
codemage66 <kingstar3380@gmail.com>
2020-10-28 12:17:07 +0100

    insert data to database

--- src/main/java/io/github/manuelernesto/algafoodapi/jpa/CadastroCozinha.java
+++ src/main/java/io/github/manuelernesto/algafoodapi/jpa/CadastroCozinha.java
@@ -2,6 +2,7 @@ package io.github.manuelernesto.algafoodapi.jpa;
 
 import io.github.manuelernesto.algafoodapi.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,6 +18,11 @@ public class CadastroCozinha {
     public List<Cozinha> listar() {
         return manager.createQuery("FROM Cozinha", Cozinha.class)
                 .getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        return manager.merge(cozinha);
     }
 
 }
---
+++ src/main/java/io/github/manuelernesto/algafoodapi/jpa/IncluirCozinhaMain.java
@@ -0,0 +1,41 @@
+package io.github.manuelernesto.algafoodapi.jpa;
+
+import io.github.manuelernesto.algafoodapi.AlgafoodApiApplication;
+import io.github.manuelernesto.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class IncluirCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                        .web(WebApplicationType.NONE)
+                        .run(args);
+
+        var cadastroCozinha = context.getBean(CadastroCozinha.class);
+
+        var cozinha1 = new Cozinha();
+        var cozinha2 = new Cozinha();
+        cozinha1.setNome("Italiana");
+        cozinha2.setNome("Japonesa");
+
+
+        var cozinhaSalva1 = cadastroCozinha.adicionar(cozinha1);
+        var cozinhaSalva2 = cadastroCozinha.adicionar(cozinha2);
+
+        System.out.println("Cozinha Salva1: " + cozinhaSalva1.getNome());
+        System.out.println("Cozinha Salva2: " + cozinhaSalva2.getNome());
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
