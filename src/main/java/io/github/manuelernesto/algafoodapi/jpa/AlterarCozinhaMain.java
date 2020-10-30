975d044af64ec9141ddd265242b43a0a19861cf5
codemage66 <kingstar3380@gmail.com>
2020-10-30 21:07:30 +0100

    update data to database

--- src/main/java/io/github/manuelernesto/algafoodapi/jpa/AlterarCozinhaMain.java
+++ src/main/java/io/github/manuelernesto/algafoodapi/jpa/AlterarCozinhaMain.java
@@ -1,11 +1,12 @@
 package io.github.manuelernesto.algafoodapi.jpa;
 
 import io.github.manuelernesto.algafoodapi.AlgafoodApiApplication;
+import io.github.manuelernesto.algafoodapi.domain.model.Cozinha;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class ConsultaCozinhaMain {
+public class AlterarCozinhaMain {
     public static void main(String[] args) {
         ApplicationContext context =
                 new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -14,12 +15,13 @@ public class ConsultaCozinhaMain {
 
         var cadastroCozinha = context.getBean(CadastroCozinha.class);
 
-        var cozinhas = cadastroCozinha.listar();
+        var cozinha = cadastroCozinha.porId(1L);
+        cozinha.setNome("Dadox");
 
+        cozinha = cadastroCozinha.adicionar(cozinha);
 
-        for (var cozinha : cozinhas) {
-            System.out.println(cozinha.getNome());
-        }
+        var toPrint = String.format("Cozinha: %s - %s", cozinha.getId(), cozinha.getNome());
+        System.out.println(toPrint);
 
     }
 }
