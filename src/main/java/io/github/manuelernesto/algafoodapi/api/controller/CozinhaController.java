66d66dabff15b3f48511049171177456043c27f2
codemage66 <kingstar3380@gmail.com>
2020-10-31 21:32:26 +0100

    add api to get all cozinhas

---
+++ src/main/java/io/github/manuelernesto/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package io.github.manuelernesto.algafoodapi.api.controller;
+
+import io.github.manuelernesto.algafoodapi.domain.model.Cozinha;
+import io.github.manuelernesto.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    private final CozinhaRepository repository;
+
+    public CozinhaController(CozinhaRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Cozinha> list() {
+        return repository.findAll();
+    }
+}
