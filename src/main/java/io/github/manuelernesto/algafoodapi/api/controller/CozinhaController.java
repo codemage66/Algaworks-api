24b435b1d9facc48b02e576ff07cfa31ffffd0e7
codemage66 <kingstar3380@gmail.com>
2020-11-03 22:51:25 +0100

    method to update the kitchen to database via rest api

--- src/main/java/io/github/manuelernesto/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/io/github/manuelernesto/algafoodapi/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package io.github.manuelernesto.algafoodapi.api.controller;
 import io.github.manuelernesto.algafoodapi.api.model.CozinhasXMLWrapper;
 import io.github.manuelernesto.algafoodapi.domain.model.Cozinha;
 import io.github.manuelernesto.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -43,6 +44,18 @@ public class CozinhaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha save(@RequestBody Cozinha cozinha) {
         return repository.add(cozinha);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cozinha> update(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+        var cozinhaActual = repository.findByID(id);
+        if (cozinhaActual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaActual, "id");
+            cozinhaActual = repository.add(cozinhaActual);
+            return ResponseEntity.ok(cozinhaActual);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
 
