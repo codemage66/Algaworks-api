b6702217574cddca5e30654a51ef1802cf8ee909
codemage66 <kingstar3380@gmail.com>
2020-11-11 14:15:58 +0100

    add endpoint to update state

--- src/main/java/io/github/manuelernesto/algafoodapi/api/controller/EstadoController.java
+++ src/main/java/io/github/manuelernesto/algafoodapi/api/controller/EstadoController.java
@@ -3,6 +3,7 @@ package io.github.manuelernesto.algafoodapi.api.controller;
 import io.github.manuelernesto.algafoodapi.domain.model.Estado;
 import io.github.manuelernesto.algafoodapi.domain.repository.EstadoRepository;
 import io.github.manuelernesto.algafoodapi.domain.services.CadastroEstadoService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,5 +31,25 @@ public class EstadoController {
     public ResponseEntity<Estado> save(@RequestBody Estado estado) {
         estado = cadastroEstadoService.save(estado);
         return ResponseEntity.status(HttpStatus.CREATED).body(estado);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estado> find(@PathVariable Long id) {
+        var estado = estadoRepository.findByID(id);
+        if (estado != null)
+            return ResponseEntity.ok(estado);
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Estado estado) {
+        var estadoAtual = estadoRepository.findByID(id);
+        if (estadoAtual != null) {
+            BeanUtils.copyProperties(estado, estadoAtual, "id");
+            estadoAtual = cadastroEstadoService.save(estadoAtual);
+            return ResponseEntity.ok(estadoAtual);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
