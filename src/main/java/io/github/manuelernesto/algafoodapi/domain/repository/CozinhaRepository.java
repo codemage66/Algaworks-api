88132f5bc315cc030a25ec0bfeb951d9c3c15b30
codemage66 <kingstar3380@gmail.com>
2021-02-07 19:17:06 +0100

    using jpa prefix

--- src/main/java/io/github/manuelernesto/algafoodapi/domain/repository/CozinhaRepository.java
+++ src/main/java/io/github/manuelernesto/algafoodapi/domain/repository/CozinhaRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
     List<Cozinha> findByNomeContaining(String name);
+
+    Optional<Cozinha> findByNome(String name);
+
+    boolean existsByNome(String name);
 }
